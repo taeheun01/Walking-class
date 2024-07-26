f44089d8c4b1a70bb74702da49f5dc600dd6222a
taeheun01 <xogms0130@gmail.com>
2024-07-26 11:50:26 +0900

    Add files via upload

---
+++ 240726/2.js
@@ -0,0 +1,13 @@
+function checkAge(age) {
+    if (age >= 19) {
+        console.log("안녕하세요")
+    } else { 
+        console.log("나가세요")
+    }
+    
+    }
+    
+    // 함수 실행
+
+    checkAge(20);
+    checkAge(12);
