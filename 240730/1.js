c2c6276357aacb53e72c9b85e4c26fe45a833dd6
taeheun01 <xogms0130@gmail.com>
2024-07-30 12:51:07 +0900

    Delete 240730 directory

--- 240730/1.js
+++
@@ -1,9 +0,0 @@
-let shoppingList = ['우유', '계란', '아이스크림', '목살'];
-
-let removed = shoppingList.pop();
-shoppingList.push('삼겹살');
-// 여기에 코드를 작성하세요.
-
-
-console.log(shoppingList);
-// 출력 : [ '우유', '계란', '아이스크림', '삼겹살' ]node
